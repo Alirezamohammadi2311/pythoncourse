4ad963a3ec0ea64601e917868f3278ecb96295e5
Alirezamohammadi2311 <99633261+Alirezamohammadi2311@users.noreply.github.com>
2022-02-28 00:08:14 +0330

    Add files via upload

---
+++ Question 6.py
@@ -0,0 +1,13 @@
+x = 2
+print("x")
+# reshte x ro neshoon khahad dad
+print('x')
+# reshte x ro neshoon khahad dad
+print(x)
+# adad 2 ro neshoon khahad dad
+print("x+1")
+# daghighan ebarate x+1 ro neshoon khahad dad
+print(x + 1)
+# adad 3 ra neshoon khahad dad
+print('x' + 1)
+# error khahad dad zira nemitavan yek reshte ra ba adad jam kard
