57fbe7434f8509b501a014a8a115bf716be0a3f9
Alirezamohammadi2311 <99633261+Alirezamohammadi2311@users.noreply.github.com>
2022-03-14 00:13:54 +0330

    Add files via upload

---
+++ Eslahie code.py
@@ -0,0 +1,15 @@
+#code eslah shode
+
+MAX = 24
+n = 1
+
+while n <= MAX:
+    factor = 1
+    print(end=str(n) +': ')
+    while factor <= n:
+        if n % factor == 0:
+            print(factor, end=' ')
+        factor += 1
+    print()
+    n += 1
+    
